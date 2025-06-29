9ffe44c98697336048d45334e95b68480df525d0
deryabeyoglukarahan <derya.beyoglu1@gmail.com>
2025-06-29 15:55:07 +0300

    new file added

---
+++ task11.sql
@@ -0,0 +1,23 @@
+--actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+SELECT DISTINCT first_name FROM actor UNION SELECT DISTINCT first_name FROM customer ORDER BY first_name;
+
+--actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+SELECT DISTINCT first_name FROM actor INTERSECT SELECT DISTINCT first_name FROM customer ORDER BY first_name;
+
+--actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+SELECT DISTINCT first_name FROM actor EXCEPT SELECT DISTINCT first_name FROM customer ORDER BY first_name;
+
+--İlk 3 sorguyu tekrar eden veriler için de yapalım.
+SELECT first_name FROM actor UNION ALL
+SELECT first_name FROM customer
+ORDER BY first_name;
+
+SELECT a.first_name FROM actor a
+JOIN customer c ON a.first_name = c.first_name
+ORDER BY a.first_name;
+
+SELECT first_name FROM actor
+WHERE first_name NOT IN (SELECT DISTINCT first_name FROM customer)
+ORDER BY first_name;
+
+
